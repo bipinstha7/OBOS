c91c8ea4d6ce918768d111f82f00270a25fd3f95
bipinstha7 <bipin.shrestha901@gmail.com>
2018-09-03 23:18:51 +0545

    remmove old vue cli and install new one

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,29 @@
 <template>
   <div id="app">
-    <HelloWorld msg="This app is on early development stage !!!"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
