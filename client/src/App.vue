77d3f7969a100a57e57a609e060ad6ac9385fbea
bipinstha7 <bipin.shrestha901@gmail.com>
2018-09-05 20:58:04 +0545

    refactor App.vue css codes

--- client/src/App.vue
+++ client/src/App.vue
@@ -18,12 +18,11 @@
 }
 #nav {
   padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
+
+  // components would get the router-link-active class when the path is selected
+ .router-link-exact-active {
       color: #42b983;
-    }
+
   }
 }
 </style>
