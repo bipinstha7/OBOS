9da576212001795b6a2cb2436ff52b6249ffff52
bipinstha7 <bipin.shrestha901@gmail.com>
2018-09-08 22:47:06 +0545

    format code based on prettier extension

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      This app is on building phase
-    </p>  
-  </div>
+	<div class="hello">
+		<h1>{{ msg }}</h1>
+		<p>
+			This app is on building phase
+		</p>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+	export default {
+	    name: 'HelloWorld',
+	    props: {
+	        msg: String
+	    }
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Online Burger Ordering System"/>
-  </div>
+	<div class="home">
+		<HelloWorld msg="Welcome to Online Burger Ordering System" />
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+	// @ is an alias to /src
+	import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+	    name: 'home',
+	    components: {
+	        HelloWorld
+	    }
+	}
 </script>
