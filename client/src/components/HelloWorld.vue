3fd14adde87bb69c72abfb6835198fbfdae3beb5
bipinstha7 <bipin.shrestha901@gmail.com>
2018-09-20 00:29:57 +0545

    add navbar components and burger image

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,5 +1,8 @@
 <template>
 	<div class="hello">
+		<burger-toolbar></burger-toolbar>
+		<!-- <navigation-items></navigation-items> -->
+		
 		<h1>{{ msg }}</h1>
 		<p>
 			This app is on building phase
@@ -8,12 +11,18 @@
 </template>
 
 <script>
-	export default {
-	    name: 'HelloWorld',
-	    props: {
-	        msg: String
-	    }
+import Toolbar from './Toolbar/Toolbar.vue'
+import NavigationItems from './NavigationItems/NavigationItems.vue'
+export default {
+	name: 'HelloWorld',
+	components: {
+		'burger-toolbar': Toolbar,
+		'navigation-items': NavigationItems
+	},
+	props: {
+		msg: String
 	}
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
