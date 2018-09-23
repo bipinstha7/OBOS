66fc20ed8b4319638ca174245cf3e1d6d581e295
bipinstha7 <bipin.shrestha901@gmail.com>
2018-09-23 12:42:59 +0545

    add new navbar

---
+++ src/components/DrawerToggle/DrawerToggle.vue
@@ -0,0 +1,52 @@
+<template>
+    <div @Click="ToggleSideDrawer" class="DrawerToggle">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div> 
+</template>
+<script>
+export default {
+    data() {
+        return {
+            showSideDrawer: false
+        }
+    },
+
+    methods: {
+        ToggleSideDrawer() {
+            this.showSideDrawer = true
+        }
+    }
+}
+
+
+</script>
+
+<style scoped>
+.DrawerToggle {
+    width: 40px;
+    height: 100%;
+    display: flex;
+    flex-flow: column;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px 0;
+    box-sizing: border-box;
+    cursor: pointer;
+}
+
+.DrawerToggle div {
+    width: 90%;
+    height: 3px;
+    background-color: white;
+}
+
+@media (min-width: 500px) {
+  .DrawerToggle {
+      display: none;
+  }
+}
+
+</style>
+
