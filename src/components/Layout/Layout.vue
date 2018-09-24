222d35e563cd9f40750e309bdc7c335c14a544c9
bipinstha7 <bipin.shrestha901@gmail.com>
2018-09-25 01:21:36 +0545

    create Layout vue for backdrop

---
+++ src/components/Layout/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <burger-toolbar></burger-toolbar>
+    </div>
+</template>
+<script>
+    import Toolbar from '../Toolbar/Toolbar.vue'
+
+    export default {
+        name: 'Layout',
+        components: {
+            'burger-toolbar': Toolbar
+        },
+    }
+</script>
