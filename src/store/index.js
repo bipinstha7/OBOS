c5f0808aa6c93a93b7cf351c30cfc0dc13154c13
Obippin <bipin@otonomis.com>
2018-11-15 21:05:10 +0545

    and and remove ingredients

--- src/store/index.js
+++ src/store/index.js
@@ -12,6 +12,43 @@ export default new Vuex.Store({
             meat: 0,
             bacon: 0
         },
+        INGREDIENT_PRICES: {
+            salad: 0.5,
+            cheese: 0.4,
+            meat: 1.3,
+            bacon: 0.7
+        },
+        totalPrice: 4,
+    },
+
+    getters: {
+        getIngredients(state) {
+            return state.ingredients
+        },
+        getIngredientsPrice(state) {
+            return state.INGREDIENT_PRICES
+        },
+        getTotalPrice(state) {
+            return state.totalPrice
+        }
+    },
+
+    mutations: {
+        addPriceAndIngredients(state, payload) {
+            state.ingredients[payload] += 1
+        },
+        removePriceAndIngredients(state, payload) {
+            state.ingredients[payload] -= 1
+        }
+    },
+
+    actions: {
+        addPriceAndIngredients({commit}, payload) {
+            commit('addPriceAndIngredients', payload)
+        },
+        removePriceAndIngredients({commit}, payload) {
+            commit('removePriceAndIngredients', payload)
+        }
     }
 
 
