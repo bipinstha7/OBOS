f3fa900ed7a6d6a4bb2b6d20b5675a370a6f832c
bipinstha7 <bipin.shrestha901@gmail.com>
2018-11-27 20:55:10 +0545

    mongodb connection

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+// connect to mongodb/mlab
+mongoose.connect('mongodb://obos:obos1212@ds119024.mlab.com:19024/obos')
+  .then(() => console.log("mongodb/mlab connected"))
+  .catch(err => console.log("Error connecting mongodb/mlab:", err));
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
