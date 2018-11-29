6162cc985b072105f7e4989c161c51d5774e3663
bipinstha7 <bipin.shrestha901@gmail.com>
2018-11-29 20:12:51 +0545

    update ingredients

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -19,11 +19,27 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     Ingredients.create(req.body.payload)
     .then(result => res.send({
-        message: 'Created Successfully'
+        message: 'Created Successfully',
+        result: result
     }))
     .catch(result => {
         res.status(500).json({
             message: 'Can Not Store Ordered Ingredients. Something Bad Happened',
+            err: err
+        })
+    })
+})
+
+// update ordered ingredients
+router.post('/:id', (req, res) => {
+    Ingredients.findByIdAndUpdate(req.params.id, req.body.payload)
+    .then(result => res.send({
+        message: 'Updated Successfully',
+        result: result
+    }))
+    .catch(err => {
+        res.status(500).json({
+            message: 'Can Not Update Ordered Ingredients. Something Bad Happened',
             err: err
         })
     })
