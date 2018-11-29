a87e0c2dbb186958c3e1e729f64f2347fdbb400d
bipinstha7 <bipin.shrestha901@gmail.com>
2018-11-29 20:22:26 +0545

    use ingredients router in index

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+const ingredientsRouter = require('./routes/ingredients')
+
 // connect to mongodb/mlab
 mongoose
 	.connect('mongodb://obos:obos1212@ds119024.mlab.com:19024/obos')
 	.then(() => console.log('mongodb/mlab connected'))
 	.catch(err => console.log('Error connecting mongodb/mlab:', err))
+
+
+app.use('/ingredients', ingredientsRouter)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
