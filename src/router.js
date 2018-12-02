ba29b865042a56aca8108fce587b0822f33cd37c
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-02 21:43:30 +0545

    home and order route

--- src/router.js
+++ src/router.js
@@ -1,24 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Order from './route/order.vue'
+import Home from './components/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [
         {
+            path: '/',
+            name: 'Home',
+            component: Home
+        },
+        {
             path: '/order',
             name: 'order',
             component: Order
-        },
-        {
-            path: '/about',
-            name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import(/* webpackChunkName: "about" */ './route/order.vue')
         }
     ]
 })
