676e7afe9572134a2a0564bea462e9a66e143a9a
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-02 23:27:29 +0545

    heroku base url and static path to dist file

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import store from './store'
 
 Vue.config.productionTip = false
 
-window.BASE_URL = 'http://localhost:3000'
+window.BASE_URL = process.env.baseURL || 'http://localhost:3000'
 window.axios = require('axios')
 window.Joi = require('joi')
 
