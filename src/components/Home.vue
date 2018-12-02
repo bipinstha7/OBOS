cf027def43bc18a81f55e97857f8d11c3dc95844
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-02 21:41:45 +0545

    Home layout

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<!-- <burger-layout> -->
+            <burger-builder></burger-builder>
+        <!-- </burger-layout> -->
+	</div>
+</template>
+
+<script>
+    import Layout from './Layout/Layout.vue'
+    import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder.vue'
+
+    export default {
+        components: {
+            'burger-layout': Layout,
+            // 'burger': Burger,
+            'burger-builder': BurgerBuilder
+        },
+    }
+</script>
