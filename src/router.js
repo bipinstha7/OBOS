3f350783f94526419acdeff2826d0ba24d2cbd59
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-02 18:02:24 +0545

    add order route

--- src/router.js
+++ src/router.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Order from './route/order.vue'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [
         {
-            path: '/',
-            name: 'home',
-            component: Home
+            path: '/order',
+            name: 'order',
+            component: Order
         },
         {
             path: '/about',
@@ -18,7 +18,7 @@ export default new Router({
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
             component: () =>
-                import(/* webpackChunkName: "about" */ './views/About.vue')
+                import(/* webpackChunkName: "about" */ './route/order.vue')
         }
     ]
 })
