3d8d27655925ad7746c0fce441d23cae96f53bf1
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-05 20:32:39 +0545

    edit page

---
+++ src/route/edit.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="form-group row">
+            <label for="email" class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-10">
+            <input type="email" class="form-control" id="email" placeholder="Email">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="Salad" class="col-sm-2 col-form-label">Salad</label>
+            <div class="col-sm-10">
+            <input type="number" class="form-control" id="Salad" placeholder="Password">
+            </div>
+        </div>
+		 <div class="form-group row">
+            <label for="cheese" class="col-sm-2 col-form-label">Cheese</label>
+            <div class="col-sm-10">
+            <input type="number" class="form-control" id="cheese" placeholder="cheese">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="meat" class="col-sm-2 col-form-label">Meat</label>
+            <div class="col-sm-10">
+            <input type="number" class="form-control" id="meat" placeholder="Password">
+            </div>
+        </div>
+		<div class="form-group row">
+            <label for="bacon" class="col-sm-2 col-form-label">Bacon</label>
+            <div class="col-sm-10">
+            <input type="number" class="form-control" id="bacon" placeholder="Password">
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-sm-10">
+            <button type="submit" class="btn btn-primary">Edit</button>
+            </div>
+        </div>
+    </div>
+</template>
