c41cbcf6930cea9a7495cb78c466782fb9c4a1c8
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-06 20:08:50 +0545

    edit data model

--- src/route/edit.vue
+++ src/route/edit.vue
@@ -3,31 +3,31 @@
         <div class="form-group row">
             <label for="email" class="col-sm-2 col-form-label">Email</label>
             <div class="col-sm-10">
-            <input type="email" class="form-control" id="email" placeholder="Email">
+            <input type="email" class="form-control" id="email" placeholder="Email" v-model="email">
             </div>
         </div>
         <div class="form-group row">
             <label for="Salad" class="col-sm-2 col-form-label">Salad</label>
             <div class="col-sm-10">
-            <input type="number" class="form-control" id="Salad" placeholder="Password">
+            <input type="number" class="form-control" id="Salad" placeholder="Password" v-model="ingredients.salad">
             </div>
         </div>
 		 <div class="form-group row">
             <label for="cheese" class="col-sm-2 col-form-label">Cheese</label>
             <div class="col-sm-10">
-            <input type="number" class="form-control" id="cheese" placeholder="cheese">
+            <input type="number" class="form-control" id="cheese" placeholder="cheese" v-model="ingredients.cheese">
             </div>
         </div>
         <div class="form-group row">
             <label for="meat" class="col-sm-2 col-form-label">Meat</label>
             <div class="col-sm-10">
-            <input type="number" class="form-control" id="meat" placeholder="Password">
+            <input type="number" class="form-control" id="meat" placeholder="Password" v-model="ingredients.meat">
             </div>
         </div>
 		<div class="form-group row">
             <label for="bacon" class="col-sm-2 col-form-label">Bacon</label>
             <div class="col-sm-10">
-            <input type="number" class="form-control" id="bacon" placeholder="Password">
+            <input type="number" class="form-control" id="bacon" placeholder="Password" v-model="ingredients.bacon">
             </div>
         </div>
         <div class="form-group row">
@@ -39,6 +39,17 @@
 </template>
 <script>
 export default {
+    data() {
+        return {
+            ingredients: {
+                salad: 0,
+                cheese: 0,
+                meat: 0,
+                bacon: 0
+            },
+            email: ''
+        }
+    },
 	methods: {
 		editOrder() {
 			axios.put(`${BASE_URL}/ingredients/${order._id}`)
