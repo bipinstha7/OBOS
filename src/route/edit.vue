c59a04db8869efabeddd6f330250cc7bc8be0a63
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-12 18:20:08 +0545

    error message in edit router

--- src/route/edit.vue
+++ src/route/edit.vue
@@ -56,7 +56,7 @@ export default {
 				.then(result => res.send(result)
 				.catch(err => res.send({
 					message: 'Something Bad Happened',
-					err: err
+					err: err.message
 				}))
 		}
 	}
