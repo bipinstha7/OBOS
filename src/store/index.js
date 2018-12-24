5bd384973fa83e8cb56816b6378a7049ede5a252
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-24 19:36:13 +0545

    code format with prettier

--- src/store/index.js
+++ src/store/index.js
@@ -1,66 +1,65 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
-        ingredients: {
-            salad: 0,
-            cheese: 0,
-            meat: 0,
-            bacon: 0
-        },
-        INGREDIENT_PRICES: {
-            salad: 10,
-            cheese: 20,
-            meat: 30,
-            bacon: 40
-        },
-        totalPrice: 20,
-        purchasing: false,
-    },
+	state: {
+		ingredients: {
+			salad: 0,
+			cheese: 0,
+			meat: 0,
+			bacon: 0,
+		},
+		INGREDIENT_PRICES: {
+			salad: 10,
+			cheese: 20,
+			meat: 30,
+			bacon: 40,
+		},
+		totalPrice: 20,
+		purchasing: false,
+	},
 
-    getters: {
-        getIngredients(state) {
-            return state.ingredients
-        },
-        getIngredientsPrice(state) {
-            return state.INGREDIENT_PRICES
-        },
-        getTotalPrice(state) {
-            return state.totalPrice
-        }
-    },
+	getters: {
+		getIngredients(state) {
+			return state.ingredients
+		},
+		getIngredientsPrice(state) {
+			return state.INGREDIENT_PRICES
+		},
+		getTotalPrice(state) {
+			return state.totalPrice
+		},
+	},
 
-    mutations: {
-        addPriceAndIngredients(state, payload) {
-            state.ingredients[payload] += 1
-            state.totalPrice += state.INGREDIENT_PRICES[payload]
-        },
-        removePriceAndIngredients(state, payload) {
-            state.ingredients[payload] -= 1
-            state.totalPrice -= state.INGREDIENT_PRICES[payload]
-        },
-        resetStore (state) {
-            for (let ingredient in state.ingredients){
-                state.ingredients[ingredient] = 0
-            }
-            state.totalPrice = 20
-            state.purchasing = false
-	    }
-    },
+	mutations: {
+		addPriceAndIngredients(state, payload) {
+			state.ingredients[payload] += 1
+			state.totalPrice += state.INGREDIENT_PRICES[payload]
+		},
+		removePriceAndIngredients(state, payload) {
+			state.ingredients[payload] -= 1
+			state.totalPrice -= state.INGREDIENT_PRICES[payload]
+		},
+		resetStore(state) {
+			for (let ingredient in state.ingredients) {
+				state.ingredients[ingredient] = 0
+			}
+			state.totalPrice = 20
+			state.purchasing = false
+		},
+	},
 
-    actions: {
-        addPriceAndIngredients({commit}, payload) {
-            commit('addPriceAndIngredients', payload)
-        },
-        removePriceAndIngredients({commit}, payload) {
-            commit('removePriceAndIngredients', payload)
-        },
-        clearAllStore({ commit }){
-			commit("resetStore")
-		}
-    }
+	actions: {
+		addPriceAndIngredients({ commit }, payload) {
+			commit('addPriceAndIngredients', payload)
+		},
+		removePriceAndIngredients({ commit }, payload) {
+			commit('removePriceAndIngredients', payload)
+		},
+		clearAllStore({ commit }) {
+			commit('resetStore')
+		},
+	},
 })
