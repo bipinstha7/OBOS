d78df5c00b0613aa65d0df6408da0b9c482e802a
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-25 19:47:32 +0545

    format code

--- models/ingredients.js
+++ models/ingredients.js
@@ -1,54 +1,54 @@
 const mongoose = require('mongoose')
 
 const ingredientSchema = new mongoose.Schema({
-    salad: {
-        total: {
-            type: Number,
-            default: 0
-        },
-        price: {
-            type: Number,
-            default: 10
-        }
-    },
-    cheese: {
-        total: {
-            type: Number,
-            default: 0
-        },
-        price: {
-            type: Number,
-            default: 20
-        }
-    },
-    meat: {
-        total: {
-            type: Number,
-            default: 0
-        },
-        price: {
-            type: Number,
-            default: 30
-        }
-    },
-    bacon: {
-        total: {
-            type: Number,
-            default: 0
-        },
-        price: {
-            type: Number,
-            default: 40
-        }
-    },
-    total_price: {
-        type: Number,
-        default: 20
-    },
-    ordered_by: {
-        type: String,
-        required: true
-    }
+	salad: {
+		total: {
+			type: Number,
+			default: 0,
+		},
+		price: {
+			type: Number,
+			default: 10,
+		},
+	},
+	cheese: {
+		total: {
+			type: Number,
+			default: 0,
+		},
+		price: {
+			type: Number,
+			default: 20,
+		},
+	},
+	meat: {
+		total: {
+			type: Number,
+			default: 0,
+		},
+		price: {
+			type: Number,
+			default: 30,
+		},
+	},
+	bacon: {
+		total: {
+			type: Number,
+			default: 0,
+		},
+		price: {
+			type: Number,
+			default: 40,
+		},
+	},
+	total_price: {
+		type: Number,
+		default: 20,
+	},
+	ordered_by: {
+		type: String,
+		required: true,
+	},
 })
 
 module.exports = mongoose.model('Ingredient', ingredientSchema)
