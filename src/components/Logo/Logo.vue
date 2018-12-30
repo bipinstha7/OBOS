b8c5b7d57189a0b3522dc83323f92da97f4c250b
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-30 20:46:36 +0545

    format logo.vue

--- src/components/Logo/Logo.vue
+++ src/components/Logo/Logo.vue
@@ -1,19 +1,19 @@
 <template>
-     <div class="Logo" >
-        <img src="../../assets/images/burger-logo.png" alt="MyBurger"/>
-    </div>
+	<div class="Logo">
+		<img src="../../assets/images/burger-logo.png" alt="MyBurger">
+	</div>
 </template>
 <style scoped>
 .Logo {
-    background: #fff;
-    padding: 8px;
-    height: 100%;
-    box-sizing: border-box;
-    border-radius: 5px;
+	background: #fff;
+	padding: 8px;
+	height: 100%;
+	box-sizing: border-box;
+	border-radius: 5px;
 }
 
 .Logo img {
-    height: 100%;
+	height: 100%;
 }
 </style>
 
