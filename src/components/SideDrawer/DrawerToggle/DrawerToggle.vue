85d842286173523742045484520ee5fcee777299
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-31 19:25:26 +0545

    drawer toggle

--- src/components/SideDrawer/DrawerToggle/DrawerToggle.vue
+++ src/components/SideDrawer/DrawerToggle/DrawerToggle.vue
@@ -1,61 +1,60 @@
 <template>
-    <div @click="ToggleSideDrawer" class="DrawerToggle">
-        <div></div>
-        <div></div>
-        <div></div>
-    </div> 
+	<div @click="ToggleSideDrawer" class="DrawerToggle">
+		<div></div>
+		<div></div>
+		<div></div>
+	</div>
 </template>
 <script>
-import { EventBus } from "../../../event-bus.js"
-export default {
-    data() {
-        return {
-            showSideDrawer: false
-        }
-    },
+	import { EventBus } from "../../../event-bus.js"
+	export default {
+		data() {
+			return {
+				showSideDrawer: false
+			}
+		},
 
-    methods: {
-        ToggleSideDrawer() {
-            // set The value from the event bus so that toggle works
-            EventBus.$on('show-side-drawer', showSideDrawer => {
-                this.showSideDrawer = showSideDrawer
-            })
+		methods: {
+			ToggleSideDrawer() {
+				// set The value from the event bus so that toggle works
+				EventBus.$on('show-side-drawer', showSideDrawer => {
+					this.showSideDrawer = showSideDrawer
+				})
 
-            this.showSideDrawer = !this.showSideDrawer
+				this.showSideDrawer = !this.showSideDrawer
 
-            // emit the value to event bus
-            EventBus.$emit('show-side-drawer', this.showSideDrawer)
-        }
-    }
-}
+				// emit the value to event bus
+				EventBus.$emit('show-side-drawer', this.showSideDrawer)
+			}
+		}
+	}
 
 
 </script>
 
 <style scoped>
 .DrawerToggle {
-    width: 40px;
-    height: 100%;
-    display: flex;
-    flex-flow: column;
-    justify-content: space-around;
-    align-items: center;
-    padding: 10px 0;
-    box-sizing: border-box;
-    cursor: pointer;
+	width: 40px;
+	height: 100%;
+	display: flex;
+	flex-flow: column;
+	justify-content: space-around;
+	align-items: center;
+	padding: 10px 0;
+	box-sizing: border-box;
+	cursor: pointer;
 }
 
 .DrawerToggle div {
-    width: 90%;
-    height: 3px;
-    background-color: white;
+	width: 90%;
+	height: 3px;
+	background-color: white;
 }
 
 @media (min-width: 500px) {
-  .DrawerToggle {
-      display: none;
-  }
+	.DrawerToggle {
+		display: none;
+	}
 }
-
 </style>
 
