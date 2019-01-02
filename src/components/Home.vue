4ce192d0660d8a70ec3d782838651502a28cab42
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-02 19:58:30 +0545

    home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,20 @@
 <template>
 	<div>
 		<!-- <burger-layout> -->
-            <burger-builder></burger-builder>
-        <!-- </burger-layout> -->
+		<burger-builder></burger-builder>
+		<!-- </burger-layout> -->
 	</div>
 </template>
 
 <script>
-    import Layout from './Layout/Layout.vue'
-    import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder.vue'
+	import Layout from './Layout/Layout.vue'
+	import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder.vue'
 
-    export default {
-        components: {
-            'burger-layout': Layout,
-            // 'burger': Burger,
-            'burger-builder': BurgerBuilder
-        },
-    }
+	export default {
+		components: {
+			'burger-layout': Layout,
+			// 'burger': Burger,
+			'burger-builder': BurgerBuilder
+		},
+	}
 </script>
