87da616337632184beb2124718f19450e44de0a7
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-20 11:36:04 +0545

    remove unnecessary components

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,13 +5,10 @@
 </template>
 
 <script>
-	import Layout from './Layout/Layout.vue'
 	import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder.vue'
 
 	export default {
 		components: {
-			'burger-layout': Layout,
-			// 'burger': Burger,
 			'burger-builder': BurgerBuilder
 		},
 	}
